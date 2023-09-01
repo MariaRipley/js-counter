a6f97d1f1a68bddb77796a087518363ab7e0bb30
MariaRipley <roblesb.maria@gmail.com>
2023-09-01 12:22:00 +0200

    Add css and decrease function

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//Set initial count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')) {
+            count--;
+        }
+        value.textContent = count;
+    })
+});
